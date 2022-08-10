77a0171c7d271cd5d089101e57b90a860831b43c
Alex24242424 <64434822+Alex24242424@users.noreply.github.com>
2022-08-10 20:17:57 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+alert(innerWidth);
+
+function openSlideMenu() {
+  if (innerWidth > 500) {
+    document.getElementById("side-menu").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+  } else {
+    document.getElementById("side-menu").style.width = "100%";
+  }
+}
+
+function closeSlideMenu() {
+  if (innerWidth > 500) {
+    document.getElementById("side-menu").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+  } else {
+    document.getElementById("side-menu").style.width = "0";
+  }
+}
