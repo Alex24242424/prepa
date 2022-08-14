fbaf2750190e662a42f8fe261ea721fab7302f14
Alexandre Potton <64434822+Alex24242424@users.noreply.github.com>
2022-08-14 22:08:15 +0200

    Add files via upload

---
+++ js/nav-bar.js
@@ -0,0 +1,46 @@
+document.write('\
+<nav class="navbar">\
+    <div class="open-slide">\
+      <a href="#" onclick="openSlideMenu()">\
+        <img class="hamburger" src="/prepa/icones/hamburger.svg" />\
+      </a>\
+    </div>\
+    <div style="flex: 1"></div>\
+    <a href="/prepa/index.html"><img class="logo" src="/prepa/icones/etoile.svg" /></a>\
+  </nav>\
+  <div id="side-menu" class="side-nav">\
+    <a href="#" class="btn-close" onclick="closeSlideMenu()">&times;</a>\
+    <div class="liens">\
+    <a href="/prepa/index.html">Accueil</a>\
+    <a href="/prepa/sites/Maths/maths.html">Mathématiques</a>\
+    <a href="/prepa/sites/Physique/physique.html">Physique</a>\
+    <a href="/prepa/sites/IPT/IPT.html">I.P.T.</a>\
+    <a href="/prepa/sites/Fiches/fiches.html">Fiches</a>\
+    </div>\
+  </div>\
+');
+
+function openSlideMenu() {
+  let canHover = window.matchMedia('(hover: hover)').matches;
+  let orient = window.matchMedia("(orientation: landscape)").matches
+  if (canHover || orient) {
+  //if (innerWidth>700) {
+    document.getElementById("side-menu").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+  } else {
+    document.getElementById("side-menu").style.width = "100%";
+  }
+}
+
+function closeSlideMenu() {
+  let canHover = window.matchMedia('(hover: hover)').matches;
+  let orient = window.matchMedia("(orientation: landscape)").matches
+  if (canHover || orient) {
+  //if (innerWidth>700) {
+    document.getElementById("side-menu").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+  } else {
+    document.getElementById("side-menu").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+  }
+}
